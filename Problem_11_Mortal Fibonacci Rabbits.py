da386203caaba1c37be62041bac76c1cf5b7e0df
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-10-30 15:53:05 +0100

    Update Problem_11_Mortal Fibonacci Rabbits.py

--- Problem_11_Mortal Fibonacci Rabbits.py
+++ Problem_11_Mortal Fibonacci Rabbits.py
@@ -5,6 +5,22 @@ Created on Tue Oct 22 17:58:51 2024
 @author: user
 """
 def cal_rabbits(n, m):
+    """
+    Calculates the population of rabbits given `n` generations and `m` months 
+    after which rabbits die, following a modified Fibonacci sequence.
+
+    Parameters:
+    -----------
+    n : int
+        The total number of generations to simulate.
+    m : int
+        The lifespan of each rabbit in months.
+    
+    Returns:
+    --------
+    int
+        The number of rabbits in the last generation (nth generation).
+    """
     seq_rab = [1, 1]
     for i in range(n - 2):
         new_num = 0
