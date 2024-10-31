d664d4e7163062c29193c725043f6373cf5d0e67
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-10-31 22:50:11 +0100

    Update Problem_23_Enumerating kmers Lexicographically.py

--- Problem_23_Enumerating kmers Lexicographically.py
+++ Problem_23_Enumerating kmers Lexicographically.py
@@ -7,6 +7,18 @@ Created on Wed Oct 30 16:50:05 2024
 from itertools import product
 
 def organize_strings(alphabet, n):
+       """
+    Generate all lexicographic strings of a given length from a specified alphabet and save them to a file.
+
+    Args:
+        alphabet (str): A string of characters representing the alphabet to use for generating strings.
+        n (int): The desired length of the generated strings.
+
+    Saves:
+        A file named "lexicographic_list.txt" containing all possible lexicographic strings of length `n` formed from the alphabet.
+        The file also includes the total number of generated strings.
+    
+    """
     combinations = list(product(alphabet, repeat=n))
     lexicographic_list = [''.join(comb) for comb in combinations]
     
