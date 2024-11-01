bda1086eb563f4449ad0512f9bfc5a04c6bd2153
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-01 20:00:23 +0100

    Update Helper_Functions.py

--- Helper_Functions.py
+++ Helper_Functions.py
@@ -4,39 +4,43 @@ Created on Wed Oct 23 20:04:34 2024
 
 @author: user
 """
-
+from Bio import SeqIO
+from Bio.Seq import Seq
 from structures import *
 def dna_to_rna(dna):
-    """Converts a DNA sequence to an RNA sequence by replacing 'T' with 'U'."""
     return dna.replace('T', 'U')
 
 def reverse_complement(dna):
-    """Returns the reverse complement of a DNA sequence."""
     mapp = str.maketrans("ATCG", "TAGC")
     return dna.translate(mapp)[::-1]
 
 def translate_dna(dna):
-    """Translates a DNA sequence into an amino acid sequence using DNA codons."""
+    """Translates a DNA sequence into an amino acid sequence until a stop codon is reached."""
     protein = []
     
     for i in range(0, len(dna) - 2, 3): 
         codon = dna[i:i+3]
+
         amino_acid = DNA_Codons[codon]
+
         protein.append(amino_acid)
     return ''.join(protein)
 
 def translate_rna(rna):
-    """Translates an RNA sequence into an amino acid sequence using RNA codons."""
+    """Translates a DNA sequence into an amino acid sequence until a stop codon is reached."""
     protein = []
     
     for i in range(0, len(rna) - 2, 3):  
         codon = rna[i:i+3]
+
         amino_acid = RNA_Codons[codon]
+
         protein.append(amino_acid)
     return ''.join(protein)
 
-def parse_fasta(fasta_data):
-    """Parses a FASTA formatted string and returns the DNA sequence without headers."""
-    lines = fasta_data.strip().splitlines()
-    dna_sequence = ''.join(line.strip() for line in lines if not line.startswith('>'))
-    return dna_sequence
+def read_fasta(filename):
+    sequences = []
+    with open(filename, "r") as file:
+        for record in SeqIO.parse(file, "fasta"):
+            sequences.append(str(record.seq))
+    return sequences
