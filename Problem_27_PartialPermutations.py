6f9fc9e571e37411141d87cff7e2d1b3f8344907
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-03 21:17:26 +0100

    Update Problem_27_PartialPermutations.py

--- Problem_27_PartialPermutations.py
+++ Problem_27_PartialPermutations.py
@@ -13,6 +13,12 @@ k = 8
 
 
 def partial_permuate(n,k):
+    """
+Given: Positive integers n and k such that 100≥n>0 and 10≥k>0.
+
+Return: The total number of partial permutations P(n,k), modulo 1,000,000.
+
+    """
     return int((factorial(n) / factorial(n-k)) % 1000000)
 
 partial_permuate(n,k)
