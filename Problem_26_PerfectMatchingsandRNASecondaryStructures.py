994bfa967cc3f42ddd56bfb1d0bb5fd93a88f8ac
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-03 21:16:14 +0100

    Update Problem_26_PerfectMatchingsandRNASecondaryStructures.py

--- Problem_26_PerfectMatchingsandRNASecondaryStructures.py
+++ Problem_26_PerfectMatchingsandRNASecondaryStructures.py
@@ -14,6 +14,13 @@ string = "ACCCUGUGUGUAAGCUAAGCCUGGGAGCUGACUUCGAUAAGCUUCUAGUCUGAACCAUUUAACACCCGGA
 
 
 def perfect_matching(string):
+    """
+Given: An RNA string s
+ of length at most 80 bp having the same number of occurrences of 'A' as 'U' and the same number of occurrences of 'C' as 'G'.
+
+Return: The total possible number of perfect matchings of basepair edges in the bonding graph of s.
+
+    """
     AU = 0
     CG = 0
     for n in string :
