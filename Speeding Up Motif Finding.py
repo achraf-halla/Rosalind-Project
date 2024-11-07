a8adbe171d1b6b68e24294e274bc5bbed5e5dcda
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-07 20:30:54 +0100

    Add files via upload

---
+++ Speeding Up Motif Finding.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 18:38:00 2024
+
+@author: user
+"""
+
+from Helper_Functions import read_fasta
+
+s = read_fasta("rosalind_kmp.txt")[0]
+
+def shorten_motif_search(s):
+	A = [0] * len(s)
+	
+	index = 1
+	length = 0
+	
+	while index < len(s):
+		if s[index] == s[length]:
+			length += 1
+			A[index] = length
+			index += 1
+		else:
+			if length == 0:
+				A[index] = 0
+				index += 1
+			else:
+				length = A[length - 1]
+	with open("resulo.txt", "w") as f :
+            f.write(" ".join([str(x) for x in A]))
+
+shorten_motif_search(s)
+
+            
