7f691ebc2c90bf0cad28b3673b1f54169c7f3559
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-08 20:01:47 +0100

    Update Problem_28_IntroductiontoRandomStrings.py

--- Problem_28_IntroductiontoRandomStrings.py
+++ Problem_28_IntroductiontoRandomStrings.py
@@ -8,6 +8,15 @@ Created on Sun Nov  3 21:13:00 2024
 from math import log10
 
 def calculate_gc_content_probabilities(s, A):
+    """
+Given: A DNA string s of length at most 100 bp and an array A containing at most 20 numbers between 0 and 1.
+
+Return: An array B
+ having the same length as A in which B[k] represents the common logarithm of the probability that a random string constructed with the GC-content found in A[k]
+ will match s exactly.
+
+
+    """
     count_A = s.count('A')
     count_T = s.count('T')
     count_C = s.count('C')
