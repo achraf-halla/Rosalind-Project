68d7cbeb616c50cdf5317a2f2f7b8872fe2f2ae7
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-08 20:08:52 +0100

    Update Problem_33_CatalanNumbersandRNASecondaryStructures.py

--- Problem_33_CatalanNumbersandRNASecondaryStructures.py
+++ Problem_33_CatalanNumbersandRNASecondaryStructures.py
@@ -14,6 +14,10 @@ def r_complem(base1, base2):
 
 
 def count_non_crossing_matchings(s):
+    """
+    Given: An RNA string s having the same number of occurrences of 'A' as 'U' and the same number of occurrences of 'C' as 'G'. The length of the string is at most 300 bp.
+    Return: The total number of noncrossing perfect matchings of basepair edges in the bonding graph of s, modulo 1,000,000.
+    """
     if s in memo:
         return memo[s]
     
