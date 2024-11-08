57e4f1f16150a34c82773b6c689aff1724e54f49
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-08 20:12:25 +0100

    Update Problem_35_CountingPhylogeneticAncestors.py

--- Problem_35_CountingPhylogeneticAncestors.py
+++ Problem_35_CountingPhylogeneticAncestors.py
@@ -9,6 +9,11 @@ Created on Wed Nov  6 20:54:01 2024
 n = 809
 
 def num_intern_nodes(n):
+    """
+    Given: A positive integer n (3≤n≤10000).
+    
+    Return: The number of internal nodes of any unrooted binary tree having n leaves.
+    """
     
     return n -2
 
