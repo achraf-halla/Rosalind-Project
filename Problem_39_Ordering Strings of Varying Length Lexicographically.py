431d4a1aa3acb0a1fb2d7dca4507c0714cc1b1e3
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-11 19:10:44 +0100

    Update Problem_39_Ordering Strings of Varying Length Lexicographically.py

--- Problem_39_Ordering Strings of Varying Length Lexicographically.py
+++ Problem_39_Ordering Strings of Varying Length Lexicographically.py
@@ -11,6 +11,13 @@ symbols = ("C W U X I H D J G E A S").split(" ")
 
 
 def ord_var_strings(symbols, n):
+    """
+Given: A permutation of at most 12 symbols defining an ordered alphabet A and a positive integer n <= 4
+
+Return: All strings of length at most n formed from A, ordered lexicographically. 
+       (Note: As in “Enumerating k-mers Lexicographically”, alphabet order is based on the order in which the symbols are given.)
+
+    """
     store = []
     rank = {symbol: i for i, symbol in enumerate(symbols)}
     def sort_key(t):
