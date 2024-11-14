9337dbf7b35657ab1a2cc7f0b8d90c42bf6d0643
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-14 20:49:20 +0100

    Update Problem_45_Counting Subsets.py

--- Problem_45_Counting Subsets.py
+++ Problem_45_Counting Subsets.py
@@ -6,7 +6,10 @@ Created on Thu Nov 14 20:38:42 2024
 """
 
 def count_subsets(n):
-
+"""
+    Given: A positive integer n (n≤1000).
+    Return: The total number of subsets of {1,2,…,n} modulo 1,000,000.
+"""
     return 2**n  % 1000000 
 
 n= 878
