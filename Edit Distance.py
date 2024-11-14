37b7ced1407da28cdc26cf0501d0105291db5f1c
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-14 18:58:44 +0100

    Add files via upload

---
+++ Edit Distance.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 19:03:38 2024
+
+@author: user
+"""
+from Bio import SeqIO
+from Bio.Seq import Seq
+def read_fasta(filename):
+    sequences = []
+    with open(filename, "r") as file:
+        for record in SeqIO.parse(file, "fasta"):
+            sequences.append(str(record.seq))
+    return sequences
+
+def edit_distance(seq1, seq2):
+    m = len(seq1) + 1
+    n = len(seq2) + 1
+
+    tbl = {}
+    for i in range(m):
+        tbl[i, 0] = i
+    for j in range(n):
+        tbl[0, j] = j
+
+    for i in range(1, m):
+        for j in range(1, n):
+            cost = 0 if seq1[i - 1] == seq2[j - 1] else 1
+            tbl[i, j] = min(tbl[i, j - 1] + 1, tbl[i - 1, j] + 1, tbl[i - 1, j - 1] + cost)
+
+    return tbl[m - 1, n - 1]
+
+s ,t = read_fasta("ina.txt")
+print(edit_distance(s,t))
+
