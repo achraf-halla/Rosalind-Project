ceb84975d2d0bf87c7304123d0bcd372dfc15e6b
Achraf Halla <132265312+achraf-halla@users.noreply.github.com>
2024-11-21 18:50:59 +0100

    Update Problem_52_Matching Random Motifs.py

--- Problem_52_Matching Random Motifs.py
+++ Problem_52_Matching Random Motifs.py
@@ -8,6 +8,12 @@ Created on Tue Nov 19 19:06:01 2024
 from math import exp
 
 def motif_probability(N, x, s):
+    """
+    Given: A positive integer N≤100000 , a number x between 0 and 1, and a DNA string s of length at most 10 bp.
+
+    Return: The probability that if N random DNA strings having the same length as s are constructed with GC-content x (see “Introduction to Random Strings”), 
+             then at least one of the strings equals s. We allow for the same random string to be created more than once.
+    """
     AT = 0 
     GC = 0  
     for nt in s:
